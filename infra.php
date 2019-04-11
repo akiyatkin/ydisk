1e4acca0afa4f4d39779f6e93b7ea012f9cde047
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2019-04-12 03:21:16 +0400

    add redirect

--- infra.php
+++ infra.php
@@ -3,6 +3,7 @@ use akiyatkin\ydisk\Ydisk;
 use infrajs\path\Path;
 use infrajs\ans\Ans;
 use infrajs\access\Access;
+use infrajs\view\View;
 
 $conf = Ydisk::$conf;
 if (!empty($conf['checkaccess'])) {
@@ -17,4 +18,8 @@ if ($name === 'true') {
 if (isset($conf['sync'][$name])) {
 	$rule = $conf['sync'][$name];
 	Ydisk::replace($rule['site'],$rule['ysrc']);	
+}
+if ($name) {
+	$path = explode('?',$_SERVER['REQUEST_URI'])[0];
+	header('Location: '.$path);
 }
