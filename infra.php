94bec9985f9634636d6f02e26bc00481c353f1a3
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-07-17 17:31:07 +0400

    fix

--- infra.php
+++ infra.php
@@ -3,6 +3,7 @@ use akiyatkin\ydisk\Ydisk;
 use infrajs\path\Path;
 use infrajs\ans\Ans;
 use infrajs\access\Access;
+use infrajs\nostore\Nostore;
 use infrajs\view\View;
 
 $conf = Ydisk::$conf;
@@ -11,13 +12,16 @@ if (!empty($conf['checkaccess'])) {
 }
 $name = Ans::get('-ydisk');
 if ($name === 'true') {
+	Nostore::on();
 	Ydisk::replaceAll();
 }
 if (isset($conf['sync'][$name])) {
+	Nostore::on();
 	$rule = $conf['sync'][$name];
 	Ydisk::replace($rule['site'],$rule['ysrc']);	
 }
 if ($name) {
+	Nostore::on();
 	$path = explode('?',$_SERVER['REQUEST_URI'])[0];
 	header('Location: '.$path.'?-nostore=true');
 }
